7a6b575cf0b6d9a60c731849ac54c82cc432a0cd
Dan Zamylin <51865252+dzam3@users.noreply.github.com>
2024-04-22 21:39:15 -0700

    Refactoring App, add format option

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,18 +2,27 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
-import java.util.concurrent.Callable;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+import java.io.File;
+
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "checksum 4.0",
         description = "Compares two configuration files and shows a difference.")
-public class App implements Callable<Integer> {
-    @Override
-    public Integer call() { // your business logic goes here...
-        System.out.println("Hello World!");
-        return 0;
-    }
+public class App implements Runnable {
+
+    @Parameters(index = "0", description = "path to first file")
+    private File filepath1;
+    @Parameters(index = "1", description = "path to second file")
+    private File filepath2;
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
+    private String format = "stylish";
+
     public static void main(String[] args) {
-        int exitCode = new CommandLine(new App()).execute(args);
-        System.exit(exitCode);
+        new CommandLine(new App()).execute(args);
+    }
+    @Override
+    public void run() { // your business logic goes here...
+        System.out.println("Hello World!");
     }
 }
