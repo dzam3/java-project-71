5e5428229e691fdd608007a7a7f0e8b575c6b862
Dan Zamylin <51865252+dzam3@users.noreply.github.com>
2024-05-14 22:18:31 -0700

    update Differ to support yaml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
     implementation("org.apache.commons:commons-collections4:4.4")
     implementation("info.picocli:picocli:4.7.5")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.17.0")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.17.0")
     testImplementation(platform("org.junit:junit-bom:5.10.1"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
 }
--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,36 +1,63 @@
 package hexlet.code;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
 public class Differ {
+    static final List<String> YAML_EXT = List.of(".yaml", ".yml");
     public static Map<String, Object> readJson(File file) throws Exception {
         ObjectMapper mapper = new ObjectMapper();
         Map<String, Object> jsonMap = mapper.readValue(file, Map.class);
         return jsonMap;
     }
 
+    public static Map<String, Object> readYaml(File file) throws Exception {
+        ObjectMapper mapper = new YAMLMapper();
+        Map<String, Object> yamlMap = mapper.readValue(file, Map.class);
+        return yamlMap;
+    }
+
     public static String generate(File file1, File file2) {
         try {
-            Map<String, Object> jointJson = new TreeMap<>();
-            Map<String, Object> jsonMap1 = readJson(file1);
-            Map<String, Object> jsonMap2 = readJson(file2);
-            jointJson.putAll(jsonMap1);
-            jointJson.putAll(jsonMap2);
+            Map<String, Object> jointMap = new TreeMap<>();
+            String fileName1 = file1.getName().toLowerCase();
+            String fileName2 = file2.getName().toLowerCase();
+            System.out.println(fileName1);
+            String ext1 = fileName1.substring(fileName1.indexOf('.'));
+            String ext2 = fileName2.substring(fileName2.indexOf('.'));
+            Map<String, Object> map1 = new HashMap<>();
+            Map<String, Object> map2 = new HashMap<>();
+
+            if (YAML_EXT.contains(ext1) && YAML_EXT.contains(ext2)) {
+                map1.putAll(readYaml(file1));
+                map2.putAll(readYaml(file2));
+            } else if (ext1.equals(".json") && ext2.equals(".json")) {
+                map1.putAll(readJson(file1));
+                map2.putAll(readJson(file2));
+            } else {
+                throw new RuntimeException("File extension is not supported. Supported extensions: \".json\", "
+                        + YAML_EXT);
+            }
+
+            jointMap.putAll(map1);
+            jointMap.putAll(map2);
             StringBuilder result = new StringBuilder("{\n");
 
-            jointJson.forEach((key, value) -> {
-                if (value.equals(jsonMap1.get(key)) && value.equals(jsonMap2.get(key))) {
+            jointMap.forEach((key, value) -> {
+                if (value.equals(map1.get(key)) && value.equals(map2.get(key))) {
                     result.append("    " + key + ": " + value + "\n");
-                } else if (!jsonMap1.containsKey(key)) {
+                } else if (!map1.containsKey(key)) {
                     result.append("  + " + key + ": " + value + "\n");
-                } else if (!jsonMap2.containsKey(key)) {
+                } else if (!map2.containsKey(key)) {
                     result.append("  - " + key + ": " + value + "\n");
-                } else if (!value.equals(jsonMap1.get(key)) && value.equals(jsonMap2.get(key))) {
-                    result.append("  - " + key + ": " + jsonMap1.get(key) + "\n");
+                } else if (!value.equals(map1.get(key)) && value.equals(map2.get(key))) {
+                    result.append("  - " + key + ": " + map1.get(key) + "\n");
                     result.append("  + " + key + ": " + value + "\n");
                 }
             });
